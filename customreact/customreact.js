df5a73f5717efe8061f9f3b5eebd33d8c1848275
NANDINI <nandiniarora011@gmail.com>
2025-05-15 00:16:18 +0530

    main file

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -9,7 +9,7 @@ function customRender(reactElement,container){
     const domElement=document.createElement(reactElement.type)
      domElement.innerHTML=reactElement.Children
 for (const prop in reactElement.props) {
-    if (prop=='Children')continue; 
+    if (prop==='Children')continue; 
     domElement.setAttribute(prop,reactElement.props[prop])
 }
 container.appendChild(domElement)
