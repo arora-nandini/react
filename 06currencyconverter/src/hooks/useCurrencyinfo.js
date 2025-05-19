f027931f8a6a9fd5af9ea08383abebedbcad43fa
NANDINI <nandiniarora011@gmail.com>
2025-05-19 13:23:14 +0530

    CurrencyConverter

---
+++ 06currencyconverter/src/hooks/useCurrencyinfo.js
@@ -0,0 +1,15 @@
+import { useEffect,useState } from "react";
+function useCurrencyinfo(currencyCode){
+    const[data,setData]=useState({})
+useEffect(()=>{
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currencyCode}.json`)
+.then((res)=>res.json())
+.then((res)=>setData(res[currencyCode]))
+console.log(data);
+
+},[currencyCode])
+console.log(data);
+
+return data;
+}
+export default useCurrencyinfo;
