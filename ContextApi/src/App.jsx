cd254d45387cba932ee3ee5bdb801b00cfa26000
NANDINI <nandiniarora011@gmail.com>
2025-05-27 19:55:42 +0530

    context

--- ContextApi/src/App.jsx
+++ ContextApi/src/App.jsx
@@ -5,15 +5,14 @@ import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
- 
+  
 
   return (
-   <UserContextProvider>
-   <h1>I hope you have a great day</h1>
-   <Login/>
-   <Profile/>
-
-   </UserContextProvider>
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
